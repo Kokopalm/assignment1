389829d222f385dcd9e36e6dc5f7b5790eee2c75
Kokopalm <166753697+Kokopalm@users.noreply.github.com>
2024-04-13 10:25:47 +0200

    Update event_analyzer.py

--- app/src/event_analyzer.py
+++ app/src/event_analyzer.py
@@ -1,1 +1,15 @@
+class EventAnalyzer:
+    @staticmethod
+    def get_joiners_multiple_meetings(events):
+        joiners_dict = {}
+        for event in events:
+            joiners = event.get('joiners', [])
+            for joiner in joiners:
+                if joiner in joiners_dict:
+                    joiners_dict[joiner] += 1
+                else:
+                    joiners_dict[joiner] = 1
+        
+        joiners_multiple_meetings = [joiner for joiner, count in joiners_dict.items() if count >= 2]
+        return joiners_multiple_meetings
 
