a0a71e4a9fa34ec7c235ce398783f94b897a1f18
Kokopalm <166753697+Kokopalm@users.noreply.github.com>
2024-04-13 10:13:03 +0200

    Update file_storage.py

--- app/src/file_storage.py
+++ app/src/file_storage.py
@@ -1,1 +1,18 @@
+import json
 
+class EventFileManager:
+    FILE_PATH = "events.json"
+    
+    @classmethod
+    def read_events_from_file(cls):
+        try:
+            with open(cls.FILE_PATH, 'r') as file:
+                events = json.load(file)
+        except (FileNotFoundError, json.JSONDecodeError):
+            events = []
+        return events
+    
+    @classmethod
+    def write_events_to_file(cls, events):
+        with open(cls.FILE_PATH, 'w') as file:
+            json.dump(events, file)
